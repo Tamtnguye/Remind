2e3bb51495c89a7eb24458682edc04896df275fb
Tam Nguyen <tamtnguye@gmail.com>
2021-05-27 13:36:55 -0700

    first commit

---
+++ app/components/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, 
+    Text, 
+    View } from 'react-native';
+
+
+ const Main = () => {
+
+    return (
+        <View>
+            <Text>Test</Text>
+        </View>
+    );
+ };
+
+ export default Main;
+ 
